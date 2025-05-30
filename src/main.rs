4ba533d017a9ebbf28212f51b57a727cdfc1b5ff
Farewe1ll <Farewe1ll_youngjh@foxmail.com>
2025-05-30 20:29:50 +0800

    init cli.rs and upd main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use clap::Parser;
+mod cli;
+
 #[tokio::main]
-async fn main(){
-    println!("Hello, world!");
+async fn main() -> anyhow::Result<()> {
+	println!("Hello, world!");
+
+	let args = cli::Cli::parse();
+	match args.command {
+		cli::Commands::Ask {query} => {
+			let answer = "haha";
+			println!("Answer: {}", answer);
+		},
+		cli::Commands::Remember {content} => {
+			println!("hey, please remember!")
+		}
+	}
+	Ok(())
 }
